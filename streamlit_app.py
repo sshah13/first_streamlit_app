7f078cbee2c5cb537c39e27734f64d24124f000d
sshah13 <67875929+sshah13@users.noreply.github.com>
2022-09-30 13:29:13 -0400

    Update streamlit_app.py
    added smoothie list

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,5 @@
 import streamlit
+import pandas
 streamlit.title('My parents new healthy diner')
    
 streamlit.header('Breakfast Menu')
@@ -7,4 +8,6 @@ streamlit.text('🥗 Kale, Spinach & Rocket smoothie')
 streamlit.text('🐔 Hard-Boiled free-range egg')
 streamlit.text('🥑🍞 Avocado Toast')
 
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+streamlit.dataframe(my_fruit_list)

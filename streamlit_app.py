3e94721516a4c942b277082822738fe4949702dc
sshah13 <67875929+sshah13@users.noreply.github.com>
2022-09-30 13:08:03 -0400

    Update streamlit_app.py
    added menu items

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,4 @@
 import streamlit
 streamlit.title('My parents new healthy diner')
-streamlit.text('Breakfast Menu')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 and bluberry oatmeal' \n 'Kale, Spinach & Rocket smoothie' \n 'Hard-Boiled free-range egg')
